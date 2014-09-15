4ecaed61ab44e1c923177a48ecf749553fa9858a
Евгений Первушин <pea@lab321.ru>
2014-09-15 04:19:36 +0100

    Функция поиска ответственного по статусу

--- SugarModules/custom/include/Workflow/WFStatusAssigned.php
+++ SugarModules/custom/include/Workflow/WFStatusAssigned.php
@@ -18,6 +18,19 @@ class WFStatusAssigned {
         return (bool)($db->fetchOne($query));
     }
     
+    public static function hasAssignedUserByStatus($status, $record_id, $module, $user_id = null) {
+        global $db;
+        $query = "SELECT 1 FROM wf_status_assigned sa, wf_statuses s1
+        WHERE
+            sa.role_id = s1.role_id
+            AND sa.record_id = '$record_id' AND sa.module = '$module' AND sa.deleted = 0
+            AND s1.uniq_name='{$status}' AND s1.wf_module = '{$module}' AND s1.deleted = 0
+        ";
+        if($user_id !== null)
+            $query .= " AND sa.user_id = '$user_id' ";
+        return (bool)($db->fetchOne($query));
+    }
+    
     public static function deleteAssignedUser($role_id, $record_id, $module, $user_id = null) {
         global $db;
         $query = "UPDATE wf_status_assigned SET deleted = 1 WHERE role_id = '$role_id' AND record_id = '$record_id' AND module = '$module' AND deleted = 0";
