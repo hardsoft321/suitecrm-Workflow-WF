9d59e86c40f54e4621c46df2514b57cfa30394bb
Антоненко Денис <ads@lab321.ru>
2014-12-16 06:54:26 +0000

    Добавил признак финальный статус

--- SugarModules/modules/WFStatuses/metadata/detailviewdefs.php
+++ SugarModules/modules/WFStatuses/metadata/detailviewdefs.php
@@ -28,7 +28,7 @@ $viewdefs ['WFStatuses'] = array (  'DetailView' =>
       'lbl_information' => array (
         array ('name', 'uniq_name'),
 		array ('role_name', 'wf_module'),
-		array ('edit_role_type'),
+		array ('edit_role_type', 'isfinal'),
 		array ('front_assigned_list_function'),
 		array ('assigned_list_function', 'role2_name'),
 		array ('confirm_list_function'),
