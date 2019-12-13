6cd40a0359505a5494f2d1afaf92bddc9f7b582f
Евгений Первушин <pea@lab321.ru>
2019-12-13 17:18:09 +0600

    fix RoleUserList

--- SugarModules/custom/include/Workflow/functions/userlists/RoleUserList.php
+++ SugarModules/custom/include/Workflow/functions/userlists/RoleUserList.php
@@ -21,7 +21,7 @@ class RoleUserList extends BaseUserList
         $q = "SELECT DISTINCT users.*
               FROM users, acl_roles_users
               WHERE users.id = acl_roles_users.user_id
-                AND users.is_group = 1 AND users.status != 'Inactive'
+                AND users.status != 'Inactive'
                 AND users.deleted = 0 AND acl_roles_users.deleted = 0
                 AND acl_roles_users.role_id = '{$this->status_data[$this->statusRoleField]}'";
         if($this->additionalWhere) {
